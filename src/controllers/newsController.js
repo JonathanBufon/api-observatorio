6624a1e0e53b00220446b914c9c699143bbe5139
MyNamelsDante <djeissonben@hotmail.com>
2026-03-27 20:54:27 -0300

    criei o getByCategory e mudei as rotas de category e title para funcionar com params, testar

--- src/controllers/newsController.js
+++ src/controllers/newsController.js
@@ -1,3 +1,5 @@
+import Noticia from "../models/Noticia.js";
+
 export const getByTitle = async (req, res) => {
     try {
         const { title } = req.params; // busaca usando o titutlo definido no Model
@@ -15,6 +17,13 @@ export const getBydate = (req, res) => {
 
 };
 
-export const getByCategory = (req, res) => {
+export const getByCategory = async (req, res) => {
+    try {
+        const { category } = req.params;
+        const news = await Noticia.find({ categoria: category });
+        res.status(200).json(news);
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao buscar por categoria", error: error.message });
+    }
 
 };
--- src/routes/newsRoutes.js
+++ src/routes/newsRoutes.js
@@ -7,6 +7,6 @@ import {
 
 const router = express.Router();
 
-router.get("/get-by-title", getByTitle);
+router.get("/get-by-title:title", getByTitle);
 router.get("/get-by-date", getBydate);
-router.get("/get-by-category", getByCategory);
+router.get("/get-by-category:category", getByCategory);
