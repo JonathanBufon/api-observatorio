756009e3abe5076faf33c86e0e1078d710af0755
MyNamelsDante <djeissonben@hotmail.com>
2026-03-27 20:31:25 -0300

    criei o getByTititle, testar

--- src/controllers/newsController.js
+++ src/controllers/newsController.js
@@ -1,4 +1,13 @@
 export const getByTitle = (req, res) => {
+    try {
+        const { title } = req.params; // busaca usando o titutlo definido no Model
+        const news = await Noticia.find({
+            titulo: { $regex: title, $options: 'i'}
+        });
+        res.status(200).json(news);
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao buscar por título", error: error.menssage });
+    }
 
 };
 
