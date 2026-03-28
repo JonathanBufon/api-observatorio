5381a3e4f16acb06430923b412f28a77a760896f
Rafaelneto <rafaelneto@unochapeco.edu.br>
2026-03-27 21:03:06 -0300

    fiz a Rota e o controller do date

--- src/controllers/newsController.js
+++ src/controllers/newsController.js
@@ -2,8 +2,15 @@ export const getByTitle = (req, res) => {
 
 };
 
-export const getBydate = (req, res) => {
-
+export const getByBetweenDate = (req, res) => {
+    const { dataInicio, dataFim } = req.query;
+    const query = {
+        data_publicacao: {
+            $gte: new Date(dataInicio),
+            $lte: new Date(dataFim)
+        }
+    };
+    res.json(query)
 };
 
 export const getByCategory = (req, res) => {
--- src/routes/newsRoutes.js
+++ src/routes/newsRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import {
     getByTitle,
-    getBydate,
+    getByBetweenDate,
     getByCategory,
 } from "../controllers/newsController.js";
 
 const router = express.Router();
 
 router.get("/get-by-title", getByTitle);
-router.get("/get-by-date", getBydate);
+router.get("/get-by-date", getByBetweenDate);
 router.get("/get-by-category", getByCategory);
