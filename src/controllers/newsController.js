4b53d6addc67a794c67e1f8ab5509f19c657f66c
Rafaelneto <rafaelneto@unochapeco.edu.br>
2026-03-27 21:01:44 -0300

    Revert "fiz a Rota e o controller do date"
    This reverts commit 8893d285657078657d2cbea0019792c7d36a0722.

--- src/controllers/newsController.js
+++ src/controllers/newsController.js
@@ -2,15 +2,8 @@ export const getByTitle = (req, res) => {
 
 };
 
-export const getByBetweenDate = (req, res) => {
-    const { dataInicio, dataFim } = req.query;
-    const query = {
-        data_publicacao: {
-            $gte: new Date(dataInicio),
-            $lte: new Date(dataFim)
-        }
-    };
-    res.json(query)
+export const getBydate = (req, res) => {
+
 };
 
 export const getByCategory = (req, res) => {
